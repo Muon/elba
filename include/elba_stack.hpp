44abff10388aefbb2f72270a756923811a70c229
Mak Nazečić-Andrlon <owlberteinstein@gmail.com>
2011-07-16 22:45:32 +1000

    Wrote code generator for pushing free functions and generated wrappers for up to 8 arguments.

--- include/elba_stack.hpp
+++ include/elba_stack.hpp
@@ -98,35 +98,7 @@ public:
 		push(wrapper_creator::wrapper, 1);
 	}
 
-	template<typename T1>
-	void push(void (*func_ptr)(T1)) const
-	{
-		struct wrapper_creator
-		{
-			typedef void (*wrapped_funcptr)(T1);
-
-			static int wrapper(lua_State* L)
-			{
-				stack st(L);
-
-				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
-
-				T1 arg1;
-				st.get(arg1, 1);
-
-				st.pop(1);
-
-				function_real(arg1);
-
-				return 0;
-			}
-		};
-
-		push(reinterpret_cast<bindable_funcptr>(func_ptr));
-		push(wrapper_creator::wrapper, 1);
-	}
-
-
+// START FREE FUNC GLUE
 	template<typename R, typename T1>
 	void push(R (*func_ptr)(T1)) const
 	{
@@ -143,8 +115,6 @@ public:
 				T1 arg1;
 				st.get(arg1, 1);
 
-				st.pop(1);
-
 				st.push(function_real(arg1));
 
 				return 1;
@@ -154,6 +124,509 @@ public:
 		push(reinterpret_cast<bindable_funcptr>(func_ptr));
 		push(wrapper_creator::wrapper, 1);
 	}
+
+	template<typename T1>
+	void push(void (*func_ptr)(T1)) const
+	{
+		struct wrapper_creator
+		{
+			typedef void (*wrapped_funcptr)(T1);
+
+			static int wrapper(lua_State* L)
+			{
+				stack st(L);
+
+				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
+
+				T1 arg1;
+				st.get(arg1, 1);
+
+				function_real(arg1);
+
+				return 0;
+			}
+		};
+
+		push(reinterpret_cast<bindable_funcptr>(func_ptr));
+		push(wrapper_creator::wrapper, 1);
+	}
+
+	template<typename R, typename T1, typename T2>
+	void push(R (*func_ptr)(T1, T2)) const
+	{
+		struct wrapper_creator
+		{
+			typedef R (*wrapped_funcptr)(T1, T2);
+
+			static int wrapper(lua_State* L)
+			{
+				stack st(L);
+
+				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
+
+				T1 arg1;
+				st.get(arg1, 1);
+				T2 arg2;
+				st.get(arg2, 2);
+
+				st.push(function_real(arg1, arg2));
+
+				return 1;
+			}
+		};
+
+		push(reinterpret_cast<bindable_funcptr>(func_ptr));
+		push(wrapper_creator::wrapper, 1);
+	}
+
+	template<typename T1, typename T2>
+	void push(void (*func_ptr)(T1, T2)) const
+	{
+		struct wrapper_creator
+		{
+			typedef void (*wrapped_funcptr)(T1, T2);
+
+			static int wrapper(lua_State* L)
+			{
+				stack st(L);
+
+				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
+
+				T1 arg1;
+				st.get(arg1, 1);
+				T2 arg2;
+				st.get(arg2, 2);
+
+				function_real(arg1, arg2);
+
+				return 0;
+			}
+		};
+
+		push(reinterpret_cast<bindable_funcptr>(func_ptr));
+		push(wrapper_creator::wrapper, 1);
+	}
+
+	template<typename R, typename T1, typename T2, typename T3>
+	void push(R (*func_ptr)(T1, T2, T3)) const
+	{
+		struct wrapper_creator
+		{
+			typedef R (*wrapped_funcptr)(T1, T2, T3);
+
+			static int wrapper(lua_State* L)
+			{
+				stack st(L);
+
+				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
+
+				T1 arg1;
+				st.get(arg1, 1);
+				T2 arg2;
+				st.get(arg2, 2);
+				T3 arg3;
+				st.get(arg3, 3);
+
+				st.push(function_real(arg1, arg2, arg3));
+
+				return 1;
+			}
+		};
+
+		push(reinterpret_cast<bindable_funcptr>(func_ptr));
+		push(wrapper_creator::wrapper, 1);
+	}
+
+	template<typename T1, typename T2, typename T3>
+	void push(void (*func_ptr)(T1, T2, T3)) const
+	{
+		struct wrapper_creator
+		{
+			typedef void (*wrapped_funcptr)(T1, T2, T3);
+
+			static int wrapper(lua_State* L)
+			{
+				stack st(L);
+
+				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
+
+				T1 arg1;
+				st.get(arg1, 1);
+				T2 arg2;
+				st.get(arg2, 2);
+				T3 arg3;
+				st.get(arg3, 3);
+
+				function_real(arg1, arg2, arg3);
+
+				return 0;
+			}
+		};
+
+		push(reinterpret_cast<bindable_funcptr>(func_ptr));
+		push(wrapper_creator::wrapper, 1);
+	}
+
+	template<typename R, typename T1, typename T2, typename T3, typename T4>
+	void push(R (*func_ptr)(T1, T2, T3, T4)) const
+	{
+		struct wrapper_creator
+		{
+			typedef R (*wrapped_funcptr)(T1, T2, T3, T4);
+
+			static int wrapper(lua_State* L)
+			{
+				stack st(L);
+
+				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
+
+				T1 arg1;
+				st.get(arg1, 1);
+				T2 arg2;
+				st.get(arg2, 2);
+				T3 arg3;
+				st.get(arg3, 3);
+				T4 arg4;
+				st.get(arg4, 4);
+
+				st.push(function_real(arg1, arg2, arg3, arg4));
+
+				return 1;
+			}
+		};
+
+		push(reinterpret_cast<bindable_funcptr>(func_ptr));
+		push(wrapper_creator::wrapper, 1);
+	}
+
+	template<typename T1, typename T2, typename T3, typename T4>
+	void push(void (*func_ptr)(T1, T2, T3, T4)) const
+	{
+		struct wrapper_creator
+		{
+			typedef void (*wrapped_funcptr)(T1, T2, T3, T4);
+
+			static int wrapper(lua_State* L)
+			{
+				stack st(L);
+
+				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
+
+				T1 arg1;
+				st.get(arg1, 1);
+				T2 arg2;
+				st.get(arg2, 2);
+				T3 arg3;
+				st.get(arg3, 3);
+				T4 arg4;
+				st.get(arg4, 4);
+
+				function_real(arg1, arg2, arg3, arg4);
+
+				return 0;
+			}
+		};
+
+		push(reinterpret_cast<bindable_funcptr>(func_ptr));
+		push(wrapper_creator::wrapper, 1);
+	}
+
+	template<typename R, typename T1, typename T2, typename T3, typename T4, typename T5>
+	void push(R (*func_ptr)(T1, T2, T3, T4, T5)) const
+	{
+		struct wrapper_creator
+		{
+			typedef R (*wrapped_funcptr)(T1, T2, T3, T4, T5);
+
+			static int wrapper(lua_State* L)
+			{
+				stack st(L);
+
+				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
+
+				T1 arg1;
+				st.get(arg1, 1);
+				T2 arg2;
+				st.get(arg2, 2);
+				T3 arg3;
+				st.get(arg3, 3);
+				T4 arg4;
+				st.get(arg4, 4);
+				T5 arg5;
+				st.get(arg5, 5);
+
+				st.push(function_real(arg1, arg2, arg3, arg4, arg5));
+
+				return 1;
+			}
+		};
+
+		push(reinterpret_cast<bindable_funcptr>(func_ptr));
+		push(wrapper_creator::wrapper, 1);
+	}
+
+	template<typename T1, typename T2, typename T3, typename T4, typename T5>
+	void push(void (*func_ptr)(T1, T2, T3, T4, T5)) const
+	{
+		struct wrapper_creator
+		{
+			typedef void (*wrapped_funcptr)(T1, T2, T3, T4, T5);
+
+			static int wrapper(lua_State* L)
+			{
+				stack st(L);
+
+				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
+
+				T1 arg1;
+				st.get(arg1, 1);
+				T2 arg2;
+				st.get(arg2, 2);
+				T3 arg3;
+				st.get(arg3, 3);
+				T4 arg4;
+				st.get(arg4, 4);
+				T5 arg5;
+				st.get(arg5, 5);
+
+				function_real(arg1, arg2, arg3, arg4, arg5);
+
+				return 0;
+			}
+		};
+
+		push(reinterpret_cast<bindable_funcptr>(func_ptr));
+		push(wrapper_creator::wrapper, 1);
+	}
+
+	template<typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
+	void push(R (*func_ptr)(T1, T2, T3, T4, T5, T6)) const
+	{
+		struct wrapper_creator
+		{
+			typedef R (*wrapped_funcptr)(T1, T2, T3, T4, T5, T6);
+
+			static int wrapper(lua_State* L)
+			{
+				stack st(L);
+
+				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
+
+				T1 arg1;
+				st.get(arg1, 1);
+				T2 arg2;
+				st.get(arg2, 2);
+				T3 arg3;
+				st.get(arg3, 3);
+				T4 arg4;
+				st.get(arg4, 4);
+				T5 arg5;
+				st.get(arg5, 5);
+				T6 arg6;
+				st.get(arg6, 6);
+
+				st.push(function_real(arg1, arg2, arg3, arg4, arg5, arg6));
+
+				return 1;
+			}
+		};
+
+		push(reinterpret_cast<bindable_funcptr>(func_ptr));
+		push(wrapper_creator::wrapper, 1);
+	}
+
+	template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
+	void push(void (*func_ptr)(T1, T2, T3, T4, T5, T6)) const
+	{
+		struct wrapper_creator
+		{
+			typedef void (*wrapped_funcptr)(T1, T2, T3, T4, T5, T6);
+
+			static int wrapper(lua_State* L)
+			{
+				stack st(L);
+
+				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
+
+				T1 arg1;
+				st.get(arg1, 1);
+				T2 arg2;
+				st.get(arg2, 2);
+				T3 arg3;
+				st.get(arg3, 3);
+				T4 arg4;
+				st.get(arg4, 4);
+				T5 arg5;
+				st.get(arg5, 5);
+				T6 arg6;
+				st.get(arg6, 6);
+
+				function_real(arg1, arg2, arg3, arg4, arg5, arg6);
+
+				return 0;
+			}
+		};
+
+		push(reinterpret_cast<bindable_funcptr>(func_ptr));
+		push(wrapper_creator::wrapper, 1);
+	}
+
+	template<typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7>
+	void push(R (*func_ptr)(T1, T2, T3, T4, T5, T6, T7)) const
+	{
+		struct wrapper_creator
+		{
+			typedef R (*wrapped_funcptr)(T1, T2, T3, T4, T5, T6, T7);
+
+			static int wrapper(lua_State* L)
+			{
+				stack st(L);
+
+				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
+
+				T1 arg1;
+				st.get(arg1, 1);
+				T2 arg2;
+				st.get(arg2, 2);
+				T3 arg3;
+				st.get(arg3, 3);
+				T4 arg4;
+				st.get(arg4, 4);
+				T5 arg5;
+				st.get(arg5, 5);
+				T6 arg6;
+				st.get(arg6, 6);
+				T7 arg7;
+				st.get(arg7, 7);
+
+				st.push(function_real(arg1, arg2, arg3, arg4, arg5, arg6, arg7));
+
+				return 1;
+			}
+		};
+
+		push(reinterpret_cast<bindable_funcptr>(func_ptr));
+		push(wrapper_creator::wrapper, 1);
+	}
+
+	template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7>
+	void push(void (*func_ptr)(T1, T2, T3, T4, T5, T6, T7)) const
+	{
+		struct wrapper_creator
+		{
+			typedef void (*wrapped_funcptr)(T1, T2, T3, T4, T5, T6, T7);
+
+			static int wrapper(lua_State* L)
+			{
+				stack st(L);
+
+				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
+
+				T1 arg1;
+				st.get(arg1, 1);
+				T2 arg2;
+				st.get(arg2, 2);
+				T3 arg3;
+				st.get(arg3, 3);
+				T4 arg4;
+				st.get(arg4, 4);
+				T5 arg5;
+				st.get(arg5, 5);
+				T6 arg6;
+				st.get(arg6, 6);
+				T7 arg7;
+				st.get(arg7, 7);
+
+				function_real(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+
+				return 0;
+			}
+		};
+
+		push(reinterpret_cast<bindable_funcptr>(func_ptr));
+		push(wrapper_creator::wrapper, 1);
+	}
+
+	template<typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8>
+	void push(R (*func_ptr)(T1, T2, T3, T4, T5, T6, T7, T8)) const
+	{
+		struct wrapper_creator
+		{
+			typedef R (*wrapped_funcptr)(T1, T2, T3, T4, T5, T6, T7, T8);
+
+			static int wrapper(lua_State* L)
+			{
+				stack st(L);
+
+				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
+
+				T1 arg1;
+				st.get(arg1, 1);
+				T2 arg2;
+				st.get(arg2, 2);
+				T3 arg3;
+				st.get(arg3, 3);
+				T4 arg4;
+				st.get(arg4, 4);
+				T5 arg5;
+				st.get(arg5, 5);
+				T6 arg6;
+				st.get(arg6, 6);
+				T7 arg7;
+				st.get(arg7, 7);
+				T8 arg8;
+				st.get(arg8, 8);
+
+				st.push(function_real(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8));
+
+				return 1;
+			}
+		};
+
+		push(reinterpret_cast<bindable_funcptr>(func_ptr));
+		push(wrapper_creator::wrapper, 1);
+	}
+
+	template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8>
+	void push(void (*func_ptr)(T1, T2, T3, T4, T5, T6, T7, T8)) const
+	{
+		struct wrapper_creator
+		{
+			typedef void (*wrapped_funcptr)(T1, T2, T3, T4, T5, T6, T7, T8);
+
+			static int wrapper(lua_State* L)
+			{
+				stack st(L);
+
+				wrapped_funcptr function_real = reinterpret_cast<wrapped_funcptr>(st.get_wrapped_function());
+
+				T1 arg1;
+				st.get(arg1, 1);
+				T2 arg2;
+				st.get(arg2, 2);
+				T3 arg3;
+				st.get(arg3, 3);
+				T4 arg4;
+				st.get(arg4, 4);
+				T5 arg5;
+				st.get(arg5, 5);
+				T6 arg6;
+				st.get(arg6, 6);
+				T7 arg7;
+				st.get(arg7, 7);
+				T8 arg8;
+				st.get(arg8, 8);
+
+				function_real(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+
+				return 0;
+			}
+		};
+
+		push(reinterpret_cast<bindable_funcptr>(func_ptr));
+		push(wrapper_creator::wrapper, 1);
+	}
+// END FREE FUNC GLUE
 
 	bindable_funcptr get_wrapped_function() const;
 
