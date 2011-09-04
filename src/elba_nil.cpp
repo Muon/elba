2a4e1c08e6d0d937d42800cfb62d0c50e080a9ea
Mak Nazečić-Andrlon <owlberteinstein@gmail.com>
2011-09-04 14:53:46 +0200

    Fixed comparisons with elba:nil, T == elba::nil will now always work properly.

--- src/elba_nil.cpp
+++ src/elba_nil.cpp
@@ -7,6 +7,6 @@ namespace elba
 const nil_type nil;
 
 bool nil_type::operator==(const reference& ref) const { return ref == *this; }
-bool nil_type::operator==(const object_index& idx) const { return idx == *this; }
+bool nil_type::operator==(const object_index& idx) const { return idx.operator==(*this); }
 
 }
--- tests/nil_test.cpp
+++ tests/nil_test.cpp
@@ -1,14 +1,20 @@
 #include <elba.hpp>
 #include <cassert>
 
+#define assert_equal(a, b) assert(a == b), assert(b == a)
+#define assert_not_equal(a, b) assert(a != b), assert(b != a)
+
 int main()
 {
 	elba::state lua;
 
-	assert(elba::nil != lua);
-	assert(elba::nil != true);
-	assert(elba::nil == elba::nil);
+	assert_not_equal(elba::nil, lua);
+	assert_not_equal(elba::nil, true);
+	assert_equal(elba::nil, elba::nil);
 	assert(!elba::nil);
+	assert_not_equal(elba::nil, lua.globals);
+	assert_equal(lua.globals[1], elba::nil);
+	assert_not_equal(elba::nil, 1);
 
 	return 0;
 }
