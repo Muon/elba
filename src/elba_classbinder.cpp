fd7ebbab3c6e7bc21b5a23c8531c074c14d612a6
Mak Nazečić-Andrlon <owlberteinstein@gmail.com>
2011-10-05 18:45:55 +0200

    Make class_binder initialize in proper order.

--- src/elba_classbinder.cpp
+++ src/elba_classbinder.cpp
@@ -4,9 +4,9 @@ namespace elba
 {
 
 class_binder::class_binder(lua_State* L)
-	: metatable(L)
+	: L(L)
+	, metatable(L)
 	, methods(L)
-	, L(L)
 {
 	metatable = make_table(L);
 	metatable.set("__index", make_table(L));
