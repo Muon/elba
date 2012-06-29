b6a0628d1f6850350e40b8f56374f4450d023ff8
Mak Nazečić-Andrlon <owlberteinstein@gmail.com>
2012-06-29 20:16:57 +0200

    Use vertical iteration for elba::reference::operator()

--- include/elba_lua_functions.inc.hpp
+++ include/elba_lua_functions.inc.hpp
@@ -1,38 +1,46 @@
+#ifndef BOOST_PP_IS_ITERATING
+
+#ifndef ELBA_LUA_FUNCTIONS_INC_HPP
+#define ELBA_LUA_FUNCTIONS_INC_HPP
+
 #include <boost/preprocessor/control/expr_if.hpp>
 #include <boost/preprocessor/repetition/repeat.hpp>
 #include <boost/preprocessor/facilities/empty.hpp>
 #include <boost/preprocessor/repetition/enum.hpp>
 #include <boost/preprocessor/repetition/enum_params.hpp>
-
-#define ARGUMENT(z, n, data) const T ## n & arg ## n
-#define ARGUMENT_LIST(n) BOOST_PP_ENUM(n, ARGUMENT, BOOST_PP_EMPTY)
-
-#define PUSH_ARGUMENT(z, n, data) st.push(arg ## n);
-#define PUSH_ARGUMENTS(nargs) BOOST_PP_REPEAT(nargs, PUSH_ARGUMENT, BOOST_PP_EMPTY)
-
-#define REFERENCE_CALL(z, nargs, data) \
-	BOOST_PP_EXPR_IF(nargs, template<) \
-		BOOST_PP_ENUM_PARAMS(nargs, typename T) \
-	BOOST_PP_EXPR_IF(nargs, >) \
-	reference operator()(ARGUMENT_LIST(nargs)) \
-	{ \
-		stack st(L); \
-		st.push(*this); \
-		PUSH_ARGUMENTS(nargs) \
-		st.call(nargs, 1); \
-		reference ref(L, -1); \
-		st.pop(1); \
-		return ref; \
-	}
+#include <boost/preprocessor/iteration/iterate.hpp>
 
 #ifndef ELBA_LUA_FUNCTION_CALL_ARITY
 #define ELBA_LUA_FUNCTION_CALL_ARITY 8
 #endif
 
-BOOST_PP_REPEAT(ELBA_LUA_FUNCTION_CALL_ARITY, REFERENCE_CALL, BOOST_PP_EMPTY)
+#define BOOST_PP_ITERATION_LIMITS (0, ELBA_LUA_FUNCTION_CALL_ARITY)
+#define BOOST_PP_FILENAME_1 "elba_lua_functions.inc.hpp"
+#include BOOST_PP_ITERATE()
 
-#undef REFERENCE_CALL
-#undef PUSH_ARGUMENTS
-#undef PUSH_ARGUMENT
-#undef ARGUMENT_LIST
+#endif
+
+#else
+
+#define nargs BOOST_PP_ITERATION()
+#define ARGUMENT(z, n, data) const T ## n & arg ## n
+#define PUSH_ARGUMENT(z, n, data) st.push(arg ## n);
+
+BOOST_PP_EXPR_IF(nargs, template<)
+	BOOST_PP_ENUM_PARAMS(nargs, typename T)
+BOOST_PP_EXPR_IF(nargs, >)
+reference operator()(BOOST_PP_ENUM(nargs, ARGUMENT, BOOST_PP_EMPTY))
+{
+	stack st(L);
+	st.push(*this);
+	BOOST_PP_REPEAT(nargs, PUSH_ARGUMENT, BOOST_PP_EMPTY)
+	st.call(nargs, 1);
+	reference ref(L, -1);
+	st.pop(1);
+	return ref;
+}
+
 #undef ARGUMENT
+#undef PUSH_ARGUMENT
+
+#endif
