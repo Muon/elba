0b9303a111075d57504f0695edc310cc10c7813c
Mak Nazečić-Andrlon <owlberteinstein@gmail.com>
2012-06-29 21:00:01 +0200

    Use vertical iteration for elba::object_index::operator()

--- include/elba_lua_functions_index.inc.hpp
+++ include/elba_lua_functions_index.inc.hpp
@@ -1,32 +1,37 @@
+#ifndef BOOST_PP_IS_ITERATING
+
+#ifndef ELBA_LUA_FUNCTIONS_INDEX_INC_HPP
+#define ELBA_LUA_FUNCTIONS_INDEX_INC_HPP
+
 #include <boost/preprocessor/control/expr_if.hpp>
 #include <boost/preprocessor/repetition/repeat.hpp>
 #include <boost/preprocessor/facilities/empty.hpp>
 #include <boost/preprocessor/repetition/enum.hpp>
 #include <boost/preprocessor/repetition/enum_params.hpp>
-
-#define ARGUMENT(z, n, text) const T ## n & text ## n
-#define ARGUMENT_LIST(n) BOOST_PP_ENUM(n, ARGUMENT, arg)
-
-#define PUSH_ARGUMENT(z, n, text) st.push(text ## n);
-#define PUSH_ARGUMENTS(nargs) BOOST_PP_REPEAT(nargs, PUSH_ARGUMENT, arg)
-
-#define REFERENCE_FORWARD_CALL(z, nargs, data) \
-	BOOST_PP_EXPR_IF(nargs, template<) \
-		BOOST_PP_ENUM_PARAMS(nargs, typename T) \
-	BOOST_PP_EXPR_IF(nargs, >) \
-	reference operator()(ARGUMENT_LIST(nargs)) \
-	{ \
-		return owner_table.get<reference>(ref)(BOOST_PP_ENUM_PARAMS(nargs, arg)); \
-	}
+#include <boost/preprocessor/iteration/iterate.hpp>
 
 #ifndef ELBA_LUA_FUNCTION_CALL_ARITY
 #error ELBA_LUA_FUNCTION_CALL_ARITY must be defined.
 #endif
 
-BOOST_PP_REPEAT(ELBA_LUA_FUNCTION_CALL_ARITY, REFERENCE_FORWARD_CALL, BOOST_PP_EMPTY)
+#define BOOST_PP_ITERATION_LIMITS (0, ELBA_LUA_FUNCTION_CALL_ARITY)
+#define BOOST_PP_FILENAME_1 "elba_lua_functions_index.inc.hpp"
+#include BOOST_PP_ITERATE()
 
-#undef REFERENCE_FORWARD_CALL
-#undef PUSH_ARGUMENTS
-#undef PUSH_ARGUMENT
-#undef ARGUMENT_LIST
-#undef ARGUMENT
+#endif
+
+#else
+
+#define nargs BOOST_PP_ITERATION()
+
+#define ARGUMENT(z, n, text) const T ## n & text ## n
+
+BOOST_PP_EXPR_IF(nargs, template<)
+	BOOST_PP_ENUM_PARAMS(nargs, typename T)
+BOOST_PP_EXPR_IF(nargs, >)
+reference operator()(BOOST_PP_ENUM(nargs, ARGUMENT, arg))
+{
+	return owner_table.get<reference>(ref)(BOOST_PP_ENUM_PARAMS(nargs, arg));
+}
+
+#endif
